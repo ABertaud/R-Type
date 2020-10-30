26f1dd79b454ce3bdbc10cabff8f3ff2debb49cf
matthias virassamy <matthias.virassamy@epitech.eu>
2020-10-30 22:49:17 +0100

    client include file

---
+++ include/Client/AEntity.hpp
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2020
+** B-CPP-501-NAN-5-1-rtype-arthur.bertaud
+** File description:
+** AEntity.hpp
+*/
+
+#ifndef AENTITY_HPP_
+#define AENTITY_HPP_
+
+#include "IEntity.hpp"
+
+namespace Graphic {
+class AEntity : public Graphic::IEntity {
+    public:
+        AEntity(const Graphic::Object &type, bool bonus, const sf::Vector2f &test);
+        AEntity(const AEntity &other) = default;
+        AEntity &operator=(const AEntity &other) = default;
+        virtual ~AEntity();
+        Graphic::Object getObject() const;    // return the type of the object
+        virtual void update() = 0;   // update the object in your window
+    private:
+    protected:
+        Graphic::Object _type;
+        sf::Vector2f _pos;           // tmp a revoir le type
+        bool _bonus;
+};
+}
+
+
+#endif
---
+++ include/Client/IEntity.hpp
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2020
+** B-CPP-501-NAN-5-1-rtype-arthur.bertaud
+** File description:
+** IEntity.hpp
+*/
+
+
+#ifndef IENTITY_HPP_
+#define IENTITY_HPP_
+
+#include <SFML/Graphics.hpp>
+
+namespace Graphic {
+enum Object
+{
+    // Graphic element
+    WALL,
+    ENEMY,
+    BULLET,
+    PLAYER
+};
+
+class IEntity {
+    public:
+        virtual ~IEntity() = default;
+        virtual void update() = 0;   // update the object in your window
+        virtual Object getObject()const = 0;   // return the type of the object
+    protected:
+    private:
+};
+}
+#endif
