dd47951e9b21ec167491e633ddf97c8836f2ea91
bcoulon <benjamin.coulon@epitech.eu>
2020-11-12 09:10:26 +0100

    re update on EntityBuilder

--- src/Client/EntityBuilder.cpp
+++ src/Client/EntityBuilder.cpp
@@ -28,29 +28,29 @@ const Graphic::AEntity &EntityBuilder::createEntity(int entityId, const entityTy
 {
     if (entityType == BACKGROUND)
         return (Graphic::Background(bonus, entityPos));
-    if (entityType == P1 || entityType == P2 || entityType == P3 || entityType == P4)
+    else if (entityType == P1 || entityType == P2 || entityType == P3 || entityType == P4)
         return (Graphic::Player(entityType, bonus, entityPos));
-    if (entityType == WALL)
+    else if (entityType == WALL)
         return (Graphic::Wall(bonus, entityPos));
-    if (entityType == OBSTACLE)
+    else if (entityType == OBSTACLE)
         return (Graphic::Obstacle(bonus, entityPos));
-    if (entityType == PLAYER_SHOOT)
+    else if (entityType == PLAYER_SHOOT)
         return (Graphic::PlayerShoot(bonus, entityPos));
-    if (entityType == ALIEN)
+    else if (entityType == ALIEN)
         return (Graphic::Alien(bonus, entityPos));
-    if (entityType == ALIEN_SHOOT)
+    else if (entityType == ALIEN_SHOOT)
         return (Graphic::AlienShoot(bonus, entityPos));
-    if (entityType == UFO)
+    else if (entityType == UFO)
         return (Graphic::Ufo(bonus, entityPos));
-    if (entityType == UFO_SHOOT)
+    else if (entityType == UFO_SHOOT)
         return (Graphic::UfoShoot(bonus, entityPos));
-    if (entityType == SPACESHIP)
+    else if (entityType == SPACESHIP)
         return (Graphic::Spaceship(bonus, entityPos));
-    if (entityType == SPACESHIP_SHOOT)
+    else if (entityType == SPACESHIP_SHOOT)
         return (Graphic::SpaceshipShoot(bonus, entityPos));
-    if (entityType == PIRATE)
+    else if (entityType == PIRATE)
         return (Graphic::Pirate(bonus, entityPos));
-    if (entityType == PIRATE_SHOOT)
+    else if (entityType == PIRATE_SHOOT)
         return (Graphic::PirateShoot(bonus, entityPos));
 
 }
