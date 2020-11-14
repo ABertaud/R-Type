af6e8fee57e9beb113d93e620eb63675dcefe494
matthias virassamy <matthias.virassamy@epitech.eu>
2020-11-14 01:36:47 +0100

    shoot sprite and anim

--- src/Client/PlayerShoot.cpp
+++ src/Client/PlayerShoot.cpp
@@ -11,6 +11,15 @@
 Graphic::PlayerShoot::PlayerShoot(int id, bool bonus, const sf::Vector2f& test, const animationState& anime) : 
 Graphic::Entity(id, PLAYER_SHOOT, bonus, test, anime)
 {
+    sf::IntRect rect(0, 0, 50, 15);
+
+    _animation.insert(std::make_pair(ANIMATION_0, rect));
+    rect.left += 50;
+    _animation.insert(std::make_pair(ANIMATION_1, rect));
+    rect.left += 50;
+    _animation.insert(std::make_pair(ANIMATION_2, rect));
+    rect.left += 50;
+    _animation.insert(std::make_pair(ANIMATION_3, rect));
 }
 
 Graphic::PlayerShoot::~PlayerShoot()
