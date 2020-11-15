835e5cf90e7292b1d9e1e1d1e71893c1669defe5
matthias virassamy <matthias.virassamy@epitech.eu>
2020-11-15 20:50:14 +0100

    spaceship animation and sprite

--- src/Client/SpaceshipShoot.cpp
+++ src/Client/SpaceshipShoot.cpp
@@ -11,6 +11,21 @@
 Graphic::SpaceshipShoot::SpaceshipShoot(int id, bool bonus, const sf::Vector2f& test, const animationState& anime) :
 Graphic::Entity(id, SPACESHIP_SHOOT, bonus, test, anime)
 {
+    sf::IntRect rect(0, 0, 30, 23);
+
+    _animation.insert(std::make_pair(ANIMATION_0, rect));
+    rect.left += 30;
+    _animation.insert(std::make_pair(ANIMATION_1, rect));
+    rect.left += 30;
+    _animation.insert(std::make_pair(ANIMATION_2, rect));
+    rect.left += 30;
+    _animation.insert(std::make_pair(ANIMATION_3, rect));
+    rect.left += 30;
+    _animation.insert(std::make_pair(ANIMATION_4, rect));
+    rect.left += 30;
+    _animation.insert(std::make_pair(ANIMATION_5, rect));
+
+
 }
 
 Graphic::SpaceshipShoot::~SpaceshipShoot()
